4b450fbdfc6f12b80712d2d985776b10f793801b
Kevin Burke <kevinburke@dhcp-10-250-98-251.harvard.edu>
2026-02-06 15:40:22 -0500

    small fix: import action run link to main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import hashlib
 SIGNING_SECRET=b"hello-from-b12"
 URL="https://b12.io/apply/submission"
 
+def iso_timestamp():
+    return datetime.datetime.utcnow().isoformat(timespec="milliseconds") + "Z"
+
+
 def main():
     payload = {
         "action_run_link": os.environ["ACTION_RUN_LINK"],
@@ -15,7 +19,7 @@ def main():
         "name": "Kevin Burke",
         "repository_link": "https://github.com/kevinburke24/b12-app",
         "resume_link": "https://drive.google.com/file/d/1cKObCmppjWJeArQOPn-PQlIGuLNJEtlf/view?usp=sharing",
-        "timestamp": datetime.datetime.now(datetime.timezone.utc).isoformat(timespec="milliseconds") + "Z",
+        "timestamp": iso_timestamp(),
     }
 
     body = json.dumps(
@@ -33,10 +37,17 @@ def main():
 
     headers = {
         "Content-Type": "application/json",
-        "X-Signature-256" : f"sha256={signature}",
+        "X-Signature-256": f"sha256={signature}",
     }
 
-    response = requests.post(URL, data=body, headers=headers)
+    response = requests.post(
+        URL,
+        data=body,   # ← MUST be the same `body`
+        headers=headers,
+    )
+
+
+    response = requests.post(URL, data=body, headers=headers,)
     response.raise_for_status()
 
     data = response.json()
