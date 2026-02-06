158f528f44849fcfb5a4fdd4d17f85d8bbd61a3f
Kevin Burke <kevinburke@dhcp-10-250-98-251.harvard.edu>
2026-02-06 14:51:11 -0500

    created b12 application submission code and yml

---
+++ main.py
@@ -0,0 +1,45 @@
+import datetime
+import json
+import requests
+import hmac
+import hashlib
+
+SIGNING_SECRET=b"hello-from-b12"
+URL="https://b12.io/apply/submission"
+
+def main():
+    payload = {
+        "action_run_link": "https://github.com/YOUR_USERNAME/YOUR_REPO/actions/runs/YOUR_RUN_ID",
+        "email": "myburkek@gmail.com",
+        "name": "Kevin Burke",
+        "repository_link": "https://github.com/kevinburke24/b12-app",
+        "resume_link": "https://drive.google.com/file/d/1cKObCmppjWJeArQOPn-PQlIGuLNJEtlf/view?usp=sharing",
+        "timestamp": datetime.datatime.utcnow().isoformat(timespec="milliseconds") + "Z",
+    }
+
+    body = json.dumps(
+        payload,
+        sort_keys=True,
+        separators=(",", ":"),
+        ensure_ascii=False,
+    ).encode("utf-8")
+
+    signature = hmac.new(
+        SIGNING_SECRET,
+        body,
+        hashlib.sha25,
+    ).hexdigest()
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Signature-256" : f"sha256={signature}",
+    }
+
+    response = requests.post(URL, data=body, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+    print(response)
+
+if __name__ == "__main__":
+    main()
